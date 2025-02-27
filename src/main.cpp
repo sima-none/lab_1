baa705e3c104ab13fc2dde0abd8ec52267addcd7
sima-none <sumapakhomova@gmail.com>
2025-02-27 20:06:43 +0300

    New text

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include "utils.h"
 
 int main() {
-    int a = 5, b = 2;
+    int a, b;
     
+    std::cout << "Checkout our new features! Now you can do math" << std::endl;
+    std::cout << "Enter two numbers" << std::endl;
+    std::cin >> a >> b;
     std::cout << "Sum: " << plus(a, b) << std::endl;
     std::cout << "Diff: " << minus(a, b) << std::endl;
 
